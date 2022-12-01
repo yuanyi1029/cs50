d145531acaea7d7a76921a11cf946545541f5fcd
yuanyi <wyuanyi1029@gmail.com>
2022-12-01 19:13:34 +0800

    update

--- week4_Memory/problem_set_4/recover/recover/recover.c
+++ week4_Memory/problem_set_4/recover/recover/recover.c
@@ -5,58 +5,53 @@
 
 int main(int argc, char *argv[])
 {
+    // Declare Byte, image_pointer, image count, buffer, filename
+    typedef uint8_t BYTE;
+    FILE *ipointer = NULL;
+    int count = 0;
+    BYTE buffer[512];
+    char filename[8 * sizeof(BYTE)];
+
+    // Return 1 if 2 arguments are not given
     if (argc != 2)
     {
         printf("Usage: ./recover card.raw");
         return 1;
     }
-    //Open file for reading
-    FILE *input_file = fopen(argv[1], "r");
-    //if check Input_file pointer fail to open then REturn error code "Could not open file"
-    if (input_file == NULL)
+
+    // Define the file pointer to point to the file
+    FILE *fpointer = fopen(argv[1], "r");
+
+    // Return 1 if fpointer returns NULL (something went wrong)
+    if (fpointer == NULL)
     {
-        printf("Could not open file");
-        return 2;
+        printf("Unable to open file");
+        return 1;
     }
 
-    //declare a variable to unsigned char to store 512 chunks array
-    unsigned char buffer[512];
-
-    //for the purpose of counting of image later in the loop
-    int count_image = 0;
-
-    //An uninitialize file pointer to use to output data gotten from input file
-    FILE *output_file = NULL;
-
-    char *filename = malloc(8 * sizeof(char));
-    //char filename[8];
-
-    /*Read 512 bytes from input_file and store on the buffer*/
-    while (fread(buffer, sizeof(char), 512, input_file))
+    // Repeat until fread does not return 8 * 512 (end of file)
+    while (fread(buffer, 1, sizeof(BYTE) * 512, fpointer) == sizeof(BYTE) * 512)
     {
-        //check if bytes is start of a JPEG
+        // Check the first 4 bytes for a JPEG file
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            //write jpeg into file name in form 001.jpg, 002.jpg and so on
-            sprintf(filename, "%03i.jpg", count_image);
 
-            //open Out_file for writing
-            output_file = fopen(filename, "w");
-
-            //fwrite(buffer, sizeof(buffer), 1, output_file);
-            //count number of image found
-            count_image++;
+            // Determine filename, open a file and increase count by 1  
+            sprintf(filename, "%03i.jpg", count);
+            ipointer = fopen(filename, "w");
+            count++;
         }
-        //Check if output have been used for valid input
-        if (output_file != NULL)
+
+        // Write buffer to file if file pointer does not return NULL
+        if (ipointer != NULL)
         {
-            fwrite(buffer, sizeof(char), 512, output_file);
+            fwrite(buffer, 1, sizeof(BYTE) * 512, ipointer);
         }
 
     }
-    free(filename);
-    fclose(output_file);
-    fclose(input_file);
+    
+    fclose(ipointer);
+    fclose(fpointer);
 
     return 0;
 }
