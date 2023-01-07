3a99c9becaf77afb7c2fc43a079c9ca8a4ab526b
yuanyi <wyuanyi1029@gmail.com>
2023-01-07 10:29:55 +0800

    update

---
+++ week3_Django/lecture3/hello/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+    # return HttpResponse("Hello!")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
+    
